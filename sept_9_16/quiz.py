6bf7aab4df48993a30ec63ddfbeabbca3afb243d
Mike Lewis <MikeLewis@Mikes-MacBook-Pro-2.local>
2016-09-09 11:18:03 -0600

    sept 9

---
+++ sept_9_16/quiz.py
@@ -0,0 +1,13 @@
+### Quiz 9/9/16
+
+# First try the exercise on the bottom of this page (http://learnpython.org/en/Loops)
+
+# Next try and finish the function below
+
+def count_characters(string):
+    '''
+    INPUT: STRING
+    OUTPUT: DICT (STRING: INT}
+    Given a string, return a dictionary where the key is the letter in the string, and the value is an int.
+    '''
+    return pass
